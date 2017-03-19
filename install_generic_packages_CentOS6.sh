8edb928901617506f43c6c4e5fc13c36161c514e
AF <afoot@walmartlabs.com>
2017-03-18 17:38:54 -0700

    script updates

--- install_generic_packages_CentOS6.sh
+++ install_generic_packages_CentOS6.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 echo 
-echo "#####################################"
+echo "##########################################"
 echo "SCRIPT: $0"
-echo "Installing GENERIC Packages..."
-echo "#####################################"
+echo "Installing GENERIC Packages for CentOS6..."
+echo "##########################################"
 echo
 
 echo "Installing EPEL..."
 yum install epel-release -y > /dev/null
 
 echo "Installing Kernel Headers and Devel..."
-yum install kernel-headers kernel-devel  -y > /dev/null
+yum install kernel-headers kernel-devel-2.6.32-573.el6.x86_64 kernel-devel-2.6.32-573.8.1.el6.x86_64 -y > /dev/null
 
 echo "Installing Java JDK"
 yum install java-1.8.0-openjdk-devel -y > /dev/null
@@ -26,20 +26,19 @@ echo "Installing nfs utilities..."
 yum install bind-utils -y > /dev/null
 
 echo "Installing misc utilities..."
-yum install vim-enhanced mlocate tcpdump nc strace screen vim mailx traceroute tree telnet nmap libxml2 pciutils git -y > /dev/null
+yum install vim-enhanced  mlocate tcpdump nc strace screen vim mailx traceroute tree telnet nmap libxml2 pciutils git -y > /dev/null
 
 echo "Installing RUBY..."
-#yum --disablerepo=epel --disablerepo=rpmforge install ruby ruby-devel ruby-irb ruby-libs ruby-ri rubygems rubygems-devel rubygem-rake  -y > /dev/null
-yum install ruby ruby-devel ruby-irb ruby-libs ruby-ri rubygems rubygems-devel rubygem-rake  -y > /dev/null
+yum --disablerepo=epel --disablerepo=rpmforge install ruby ruby-devel ruby-irb ruby-libs ruby-ri rubygems rubygems-devel rubygem-rake  -y > /dev/null
 
 echo "Installing Python..."
-yum install python python-tools python-devel -y > /dev/null 
+yum install python python-tools python-devel -y > /dev/null
 
 echo "Installing Perl CPAN packages..."
 yum install perl-CPAN perl-CPANPLUS -y > /dev/null
 
 echo "Installing Perl MySQL packages..."
-yum install perl-DBI perl-DBD-MySQL perl-DBD-Pg -y > /dev/null
+yum install perl-DBI perl-DBD-MySQL perl-DBD-Pg -y > /dev/null 
 
 echo "Updating the local file database... will taka a couple of mins."
 updatedb
