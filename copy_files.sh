4ad144371b86b8beb19826af090790ae4286ba7f
AF <afoot@walmartlabs.com>
2017-04-05 10:11:59 -0700

    modified copy_files.sh

--- copy_files.sh
+++ copy_files.sh
@@ -1,10 +1,7 @@
 DEST=/usr/local/bin
-#SOURCE=/Users/afoot/learning/scripts
-SOURCE=/vagrant/shared/scripts
 FILE=treeprint.sh
 LINK=treeprint
 
-cp $SOURCE/$FILE /usr/local/bin
+cp $FILE $DEST
 chmod +x $DEST/$FILE
-ls -l $DEST/$FILE 
 ln -s $DEST/$FILE $DEST/$LINK
